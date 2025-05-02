1730aaefa469c9b2445e3d4c25013a95579c3691
Kris Claessens <59504265+claessenskris@users.noreply.github.com>
2025-05-02 16:30:35 +0200

    Add files via upload

---
+++ Installation/HairFastGAN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gcsfuse --implicit-dirs image_generator_request /pascalem/_pyt/HairFastGAN/data
+
+cd /pascalem
+
+source _pyt/bin/activate
+
+# Check if the environment is activated
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment not activated."
+    exit 1
+fi
+
+cd _pyt/HairFastGAN
+
+python3 FileSharing_Run.py --run_id HairFastGAN --bucket image_generator_request --config _config6 --token confident-coder-285618-1a0704c69644.json 
